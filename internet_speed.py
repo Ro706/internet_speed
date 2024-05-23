62a659fcb7c7d9379758304827524058044a9264
Rohit Mandal <60247178+Ro706@users.noreply.github.com>
2024-05-24 00:12:37 +0530

    Update internet_speed.py

--- internet_speed.py
+++ internet_speed.py
@@ -2,30 +2,41 @@ import psutil
 import speedtest
 from tabulate import tabulate
 from pyfiglet import Figlet
-f = Figlet(font='slant')
-print (f.renderText('internet_speed'))
 
-class Network_Details(object):
+def render_header():
+    """
+    Renders the header using Figlet with a slant font.
+    """
+    figlet = Figlet(font='slant')
+    print(figlet.renderText('Internet Speed'))
+
+class NetworkDetails:
     def __init__(self):
         self.parser = psutil.net_if_addrs()
-        self.speed_parser = speedtest.speedtest()
-        self.interfaces = self.interface()[0]
+        self.speed_parser = speedtest.Speedtest()  # Use correct capitalization for Speedtest class
+        self.interfaces = self.get_interfaces()
 
-    def interface(self):
+    def get_interfaces(self):
+        """
+        Retrieves the network interface names.
+        """
         interfaces = []
-        for interface_name,_ in self.pareser.items():
-            interface.append(str(interface_name))
+        for interface_name in self.parser.keys():  # Corrected parser attribute name
+            interfaces.append(str(interface_name))
         return interfaces
 
     def __repr__(self):
-        down = str(f"{round(self.speed_parser.download() / 1_000_000,2)}Mbps")
-        up = str(f"{round(self.speed_parser.upload() / 1_000_000,2)}Mbps")
-        interface = self.interfaces
-        data = {"Interface:":[interface],
-                "Download:":[down],
-                "Upload:": [up]}
-        table = tabulate(data,headers="keys",tablefmt="pretty")
+        download_speed = f"{round(self.speed_parser.download() / 1_000_000, 2)} Mbps"
+        upload_speed = f"{round(self.speed_parser.upload() / 1_000_000, 2)} Mbps"
+        interfaces = ', '.join(self.interfaces)
+        data = {
+            "Interface": [interfaces],
+            "Download": [download_speed],
+            "Upload": [upload_speed]
+        }
+        table = tabulate(data, headers="keys", tablefmt="pretty")
         return table
 
-if __name__=="__main__":
-    print (Network_Details())
+if __name__ == "__main__":
+    render_header()
+    print(NetworkDetails())
